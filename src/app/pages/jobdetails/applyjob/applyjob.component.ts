1a983765d832cd82561a52221d6995ebf987d2a6
Sumit Dey <sumitdeyonline@gmail.com>
2020-02-18 21:28:29 -0800

    SMTP Server

--- src/app/pages/jobdetails/applyjob/applyjob.component.ts
+++ src/app/pages/jobdetails/applyjob/applyjob.component.ts
@@ -9,9 +9,11 @@ import { ApplyJob } from 'src/app/services/firebase/applyjob/applyjob.model';
 import { AuthService } from 'src/app/services/authentication/auth.service';
 import { ApplyjobService } from 'src/app/services/firebase/applyjob/applyjob.service';
 import { UploadResume } from 'src/app/services/firebase/uploadresume/uploadresume.model';
+//import { SnotifyService } from 'ng-snotify';
 import './../../../../assets/js/smtp.js'; 
+//import './smtp.js'; 
 declare let Email: any;
-
+//var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof (XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
 @Component({
   selector: 'app-applyjob',
